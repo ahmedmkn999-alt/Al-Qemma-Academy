5b61320081f5b66472ff28dac3dbe8fe3ec9a7c6
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2026-03-04 13:28:52 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const subjectsData = [
+  {
+    id: 'arabic', title: 'اللغة العربية', img: 'https://i.postimg.cc/L6xvnk5X/من_أول_من_تكلم_اللغة_العربية.jpg',
+    teachers: [
+      { id: 't1', name: 'محمد صلاح', img: 'https://i.postimg.cc/ZRJy8gDR/FB_IMG_1770382315133.jpg' },
+      { id: 't2', name: 'رضا الفاروق', img: 'https://i.postimg.cc/c4Pg965p/1723904748_y_CL9UA.png' }
+    ]
+  },
+  {
+    id: 'english', title: 'اللغة الإنجليزية', img: 'https://i.postimg.cc/TwvBCjry/characteristics_of_a_prestigious_language_school_1_1020x642_1.jpg',
+    teachers: [
+      { id: 't3', name: 'عبقري لغة', img: 'https://i.postimg.cc/85CzPV0d/mr_tarek_hero_section_f2bf4f0e2cf0c8b0d728.png' },
+      { id: 't4', name: 'ميس مي', img: 'https://i.postimg.cc/DzS7tcJs/FB_IMG_1770382635144.jpg' },
+      { id: 't5', name: 'شريف المصري', img: 'https://i.postimg.cc/4nRGtCBW/loginbg.png' },
+      { id: 't6', name: 'انجلشاوي', img: 'https://i.postimg.cc/X7q1K1Td/splashs1.png' }
+    ]
+  },
+  {
+    id: 'physics', title: 'الفيزياء', img: 'https://i.postimg.cc/Z5RBj2mp/904.jpg',
+    teachers: [
+      { id: 't7', name: 'محمد عبد المعبود', img: 'https://i.postimg.cc/fRTXpkcm/FB_IMG_1770383335913.jpg' },
+      { id: 't8', name: 'كيلرس', img: 'https://i.postimg.cc/qR0pD6nd/FB_IMG_1770383231786.jpg' }
+    ]
+  },
+  {
+    id: 'chemistry', title: 'الكيمياء', img: 'https://i.postimg.cc/Njz4xR3j/4a228f61e5d6843a9ddd56e6cc39ef6d.jpg',
+    teachers: [
+      { id: 't9', name: 'محمد عبد الجواد', img: 'https://i.postimg.cc/rpR9ZKTR/c78ffbf64a791da67752132a9aaf7c8f127fc4f4.jpg' },
+      { id: 't10', name: 'عمرو الصيفي', img: 'https://i.postimg.cc/5tQmK6Jm/FB_IMG_1770384007129.jpg' },
+      { id: 't11', name: 'عبدالله حبشي', img: 'https://i.postimg.cc/MGfY3vwV/hero_light.webp' },
+      { id: 't12', name: 'خالد صقر', img: 'https://i.postimg.cc/hGQ0Zfn9/teacher_Bflsmq_Qf.webp' }
+    ]
+  },
+  {
+    id: 'biology', title: 'الأحياء', img: 'https://i.postimg.cc/cJjdZkVx/FB_IMG_1770384472698.jpg',
+    teachers: [
+      { id: 't13', name: 'أحمد الجوهري', img: 'https://i.postimg.cc/XJC4vcbq/89549ee43b7a31c4f350f0d32a116fbd02313c37.png' },
+      { id: 't14', name: 'محمد أيمن', img: 'https://i.postimg.cc/856NCBS7/profile_433b1d2b5e7bfd30bed5.png' }
+    ]
+  }
+];
+      
